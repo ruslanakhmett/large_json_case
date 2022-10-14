dbbafc0fb6e98c5e7d2174fa941f423653eed9f3
Ruslan Akhmetshin <sccrsccr1@gmail.com>
2022-10-14 20:30:55 +0300

    add async download and save jsons

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
-import requests
-import json
-
-#https://rdb.altlinux.org/api/export/branch_binary_packages/sisyphus
-#https://rdb.altlinux.org/api/export/branch_binary_packages/p10
+import asyncio
+import aiohttp
 
 
-req = requests.get("https://rdb.altlinux.org/api/export/branch_binary_packages/sisyphus")
-data = req.content.decode()
-req.close()
+async def fetch(sem, session, url, filename):
+    async with sem, session.get(url) as response:
+        with open(filename, "wb") as out:
+            async for chunk in response.content.iter_chunked(4096):
+                out.write(chunk)
 
-jsObj = json.loads(data)
-with open("./data_file.json", mode="w", encoding="utf8") as f:
-    f.write(json.dumps(jsObj, ensure_ascii=False))
+
+async def fetch_all(urls, loop):
+    sem = asyncio.Semaphore(12) 
+    async with aiohttp.ClientSession(loop=loop, connector=aiohttp.TCPConnector(ssl=False)) as session:
+        results = await asyncio.gather(
+            *[fetch(sem, session, url, filename) for url, filename in urls_and_filenames]
+        )
+        return results
+
+
+if __name__ == '__main__':
+
+    urls_and_filenames = [
+        ('https://rdb.altlinux.org/api/export/branch_binary_packages/sisyphus', 'sisyphus.json'),
+        ('https://rdb.altlinux.org/api/export/branch_binary_packages/p10', 'p10.json'),
+    ]
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(fetch_all(urls_and_filenames, loop))
